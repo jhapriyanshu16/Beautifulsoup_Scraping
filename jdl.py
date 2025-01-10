b9778782e17705f13ef9fd3afe4cd17ad2066b35
jhapriyanshu16 <priyanshu2015madhepur@gmail.com>
2025-01-10 22:07:59 +0530

    Successfully retrieved college fees data

--- jdl.py
+++ jdl.py
@@ -14,6 +14,12 @@ response = requests.get(url,headers=headers)
 rank_list =[]
 clg_name_list = []
 clg_link_list = []
+clg_location_list = []
+clg_approval_list = []
+clg_course_list = []
+clg_cutoff_list = []
+clg_fees_list = []
+program_name_list = []
 
 soup = BeautifulSoup(response.text,'html.parser')
 table_wrapper = soup.find('div',class_= 'jsx-4033392124 jsx-1933831621 table-wrapper')
@@ -26,10 +32,51 @@ for tr in tr_list:
         if (index == 1):
             clg_name_list.append(td.div.div.a["data-csm-title"])
             clg_link_list.append(td.div.div.a["href"])
-    # break
+            location = td.div.find('span',class_='jsx-3230181281 pr-1 location')
+            if(location):
+                clg_location_list.append(location.text.strip())
+            else:
+                clg_location_list.append('')
+            approval = td.div.find('span',class_='jsx-3230181281 approvals pl-1')
+            if(approval):
+                clg_approval_list.append(approval.text.replace("\xa0"," ").strip())
+            else:
+                clg_approval_list.append('')
+            course = td.div.find('span',class_='jsx-3230181281 course-name')
+            if(course):
+                clg_course_list.append(course.text.strip())
+            else:
+                clg_course_list.append('')
+            cutoff = td.div.find('div',class_='jsx-3230181281 col-popular-course position-relative d-inline-flex justify-space-between mt-2').find('span',class_='jsx-3230181281').find_next_sibling('span')
+            if(cutoff):
+                clg_cutoff_list.append(cutoff.text.strip())
+            else:
+                clg_cutoff_list.append('')
+        if(index == 2):
+            fees = td.a.find('span',class_="jsx-3230181281 text-lg text-green d-block font-weight-bold mb-1")
+            if(fees):
+                clg_fees_list.append(int(fees.text[2:].replace(',','').strip()))
+            else:
+                clg_fees_list.append('')
+            program = td.a.find('span',class_="jsx-3230181281 fee-shorm-form")
+            if(program):
+                program_name_list.append(program.text.strip())
+            else:
+                program_name_list.append('')
+            
+        
+            
 
-# print(rank_list)
-# print(clg_name_list)
-# print(clg_link_list)
+    break
+
+print(rank_list)
+print(clg_name_list)
+print(clg_link_list)
+print(clg_location_list)
+print(clg_approval_list)
+print(clg_course_list)
+print(clg_cutoff_list)
+print(clg_fees_list)
+print(program_name_list)
 
 
